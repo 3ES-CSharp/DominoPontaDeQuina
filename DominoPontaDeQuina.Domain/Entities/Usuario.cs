5ce079f9aa1b6b435ee88aead9a20196e66d989a
ianny2003 <iannyraquel252488@gmail.com>
2026-08-24 17:29:42 -0300

    feat: implementa modelo de dados com EF Core e SQL Server

--- DominoPontaDeQuina.Domain/Entities/Usuario.cs
+++ DominoPontaDeQuina.Domain/Entities/Usuario.cs
@@ -1,11 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DominoPontaDeQuina.Domain.Entities;
 
+[Table("Usuarios")]
 public class Usuario
 {
+    [Key]
     public Guid Id { get; set; } = Guid.NewGuid();
+
+    [Required]
+    [MaxLength(100)]
     public string Nome { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(150)]
+    [EmailAddress]
     public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(255)]
     public string HashSenha { get; set; } = string.Empty;
+
+    [Required]
     public DateTime CriadoEm { get; set; } = DateTime.UtcNow;
+
     public ICollection<Jogador> Jogadores { get; set; } = new List<Jogador>();
 }
--- DominoPontaDeQuina.Repository/Context/DominoDbContext.cs
+++ DominoPontaDeQuina.Repository/Context/DominoDbContext.cs
@@ -1,7 +1,19 @@
+using DominoPontaDeQuina.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace DominoPontaDeQuina.Repository.Context;
 
 public class DominoDbContext : DbContext
 {
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Jogador> Jogadores { get; set; }
+    public DbSet<Jogo> Jogos { get; set; }
+    public DbSet<ParticipacaoJogo> ParticipacoesJogo { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=localhost;Database=DominoDB;Trusted_Connection=True;TrustServerCertificate=True;"
+        );
+    }
 }
